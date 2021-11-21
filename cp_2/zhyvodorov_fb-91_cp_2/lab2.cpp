f21281482a82225fefc27dac9024db61c7af4d7f
Artem J <artemj10@gmail.com>
2021-11-21 15:42:54 +0200

    готово

--- cp_2/zhyvodorov_fb-91_cp_2/lab2.cpp
+++ cp_2/zhyvodorov_fb-91_cp_2/lab2.cpp
@@ -7,27 +7,11 @@ string Alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэ�
 map <char, int> index;
 map <int, char> letter;
 map <char, int> number;
+map <int, char> numberBack;
 float IndexFinde(string text, int key)
 {
     float n = text.size() * (text.size() - 1);
     float IndVid = 0;
-    /*for (int i = 0; i < text.size(); i++)
-    {
-        if ((i + key) < text.size())
-        {
-            for (int y = 0; y < key; y++)
-            {
-                number.find(text[i])->second++;
-                i++;
-            }
-            for (int z = 0; z < Alphabet.size(); z++)
-            {
-                IndVid += float((number.find(Alphabet[z])->second * (number.find(Alphabet[z])->second - 1))) / n;
-                number.find(Alphabet[z])->second = 0;
-            }
-        }
-    }
-    */
     for (int i = 0; i < text.size(); i++)
     {
         number.find(text[i])->second++;
@@ -92,19 +76,15 @@ void Task2()
         number.find(text[i])->second++;
         letters++;
     }
-   for (int i = 0; i < Alphabet.size(); i++)
-    {
-       // number.find(Alphabet[i])->second = number.find(Alphabet[i])->second / letters;
-        cout << number.find(Alphabet[i])->first << " - " << number.find(Alphabet[i])->second<<endl;
-    }
-   cout << letters << endl;
+    cout << "Длина текста - " << letters << endl;
    float Index = 0;
    for (int i = 0; i < Alphabet.size(); i++)
    {
        Index += float((number.find(Alphabet[i])->second * (number.find(Alphabet[i])->second - 1))) / float((letters * (letters - 1)));
+       number.find(Alphabet[i])->second = 0;
    }
    cout << "Индекс соответствия - " << Index << endl;
-   /* for (int i = 0; i < 25; i++)
+    for (int i = 0; i < 30; i++)
     {
         int r = 6;
         r += i;
@@ -120,7 +100,66 @@ void Task2()
         }
         cout << " Кофициент совпадений при  r=" << r << " - " << Kroneker << endl;
     }
-    */
+    string Y[17];
+    for (int i =0; i <= 16; i++)
+    {
+        string word ="";
+        for (int y = 0; y < 403; y++)
+        {
+            word +=text[i + 17 * y];
+        }
+        Y[i] = word;
+    }
+   for (int i = 0; i <= 16; i++)
+    {
+   //     cout << Y[i] << endl;
+    }
+   
+   string common="";
+   for (int i = 0; i < 16; i++)
+   {
+       string current = Y[i];
+       for (int u = 0; u < current.size(); u++)
+       {
+           number.find(current[u])->second++;
+       }
+       for (int z = 0; z < Alphabet.size(); z++)
+       {
+           numberBack[number.find(Alphabet[z])->second] = number.find(Alphabet[z])->first;
+           //cout << number.find(Alphabet[z])->first << " - " << number.find(Alphabet[z])->second << endl;
+          // cout << numberBack.find(number.find(Alphabet[z])->second)->first << " - " << numberBack.find(number.find(Alphabet[z])->second)->second << endl;
+       }
+       int max = number.find(Alphabet[0])->second;
+       number.find(Alphabet[0])->second = 0;
+       for (int z = 1; z < Alphabet.size(); z++)
+       {
+           if (number.find(Alphabet[z])->second > max) {
+               max = number.find(Alphabet[z])->second;
+           }
+           number.find(Alphabet[z])->second = 0;
+       }
+       common += numberBack.find(max)->second;
+       numberBack.clear();
+   }
+   cout <<"Most common letters: "<< common << endl;
+
+   string key = "";
+   for (int i = 0; i < common.size(); i++)
+   {
+       int a = index.find(common[i])->second - 14;
+       if (a < 0) { a = Alphabet.size() - a; }
+       key += letter.find(a % Alphabet.size())->second;
+   }
+   cout << "Key: " << key<<endl;
+   key = "возвращениеджинна";
+   string unsiphered;
+   for (int i = 0; i < text.size(); i++)
+   {
+       int a = (index.find(text[i])->second - index.find(key[i % key.size()])->second) % Alphabet.size();
+       if (a < 0) {          a = Alphabet.size() - a;      }
+       unsiphered += letter.find(a)->second;
+   }
+   cout << unsiphered;
 }
 
 int main()
@@ -133,7 +172,9 @@ int main()
         letter[i] = Alphabet[i];
         number[Alphabet[i]] = 0;
     }
+    
+  // Task1();
+  Task2();
 
-   Task1();
-  //  Task2();
+
 }
